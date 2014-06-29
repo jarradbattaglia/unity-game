5c0d47a68917a64d1166f84ea982ec2844b3c9dc
Jarrad Battaglia <jmbbat@gmail.com>
2014-06-29 17:21:06 -0400

    Initial commit

---
+++ Assets/linkController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class linkController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 transformDirection = transform.position;
+
+		if (Input.GetButton ("Fire1")) {
+			Vector3 moveToward = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			moveDirection = moveToward - transformDirection;
+			moveDirection.z = 0;
+			moveDirection.Normalize();
+		}
+	}
+}
