787b215af5f9fbe30fc55905b22c57b1214afc76
Jarrad Battaglia <jmbbat@gmail.com>
2014-06-30 14:41:52 -0400

    added some lame camera controls added some link animations

---
+++ Assets/cameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position.Set(target.transform.position.x, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/linkAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class linkAnimator : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public int framePerSecond;
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = renderer as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = (int) (Time.timeSinceLevelLoad * framePerSecond);
+		index = index % sprites.Length;
+		spriteRenderer.sprite = sprites [index];
+
+	}
+}
