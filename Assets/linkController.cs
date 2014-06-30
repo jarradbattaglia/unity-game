42bb8ed6c8eb992d017039678582cd057668d526
Robert Mruczek <rtmruczek@gmail.com>
2014-06-30 00:17:43 -0400

    you can move link around :D

--- Assets/linkController.cs
+++ Assets/linkController.cs
@@ -13,8 +13,39 @@ public class linkController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 transformDirection = transform.position;
 
+		float yvel = 0f;
+		float xvel = 0f;
+		
+		if (Input.GetKey (KeyCode.W)) {
+			yvel = 1f;
+		}
+		
+		if (Input.GetKey (KeyCode.A)) {
+			xvel = -1f;
+		}
+		
+		if (Input.GetKey (KeyCode.S)) {
+			yvel = -1f;
+		}
+		
+		if (Input.GetKey (KeyCode.D)) {
+			xvel = 1f;
+		}
+		
+		if(!Input.GetKey(KeyCode.W) && 
+		   !Input.GetKey (KeyCode.A) &&
+		   !Input.GetKey (KeyCode.S) &&
+		   !Input.GetKey (KeyCode.D)){
+			
+			xvel = 0f;
+			yvel = 0f;
+		}
+		
+		rigidbody2D.velocity = new Vector2 (xvel, yvel);
+
+		Vector3 transformDirection = transform.position;
+		
 		if (Input.GetButton ("Fire1")) {
 			Vector3 moveToward = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			moveDirection = moveToward - transformDirection;
